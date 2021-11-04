b77710837ab54c202542b13314873cd3595f67bf
Joe Webster <joseph.scott.webster@gmail.com>
2021-11-04 10:27:10 -0400

    Update search results to show specific paragraphs

--- book_viewer.rb
+++ book_viewer.rb
@@ -28,6 +28,7 @@ end
 
 get '/search' do
   @results = chapters_matching(params[:query])
+
   erb :search
 end
 
@@ -37,8 +38,8 @@ end
 
 helpers do
   def in_paragraphs(text)
-    text.split("\n\n").map do |paragraph|
-      "<p>#{paragraph}</p>"
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id=paragraph#{index}>#{line}</p>"
     end.join
   end
 
@@ -53,10 +54,15 @@ helpers do
   def chapters_matching(query)
     results = []
 
-    return results if !query || query.empty?
+    return results unless query
 
     each_chapter do |number, name, contents|
-      results << { number: number, name: name } if contents.include?(query)
+      matches = {}
+      contents.split("\n\n").each_with_index do |paragraph, index|
+        matches[index] = paragraph if paragraph.include?(query)
+      end
+
+      results << { number: number, name: name, paragraphs: matches } if matches.any?
     end
 
     results
