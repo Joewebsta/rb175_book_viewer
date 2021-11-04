b9ecd88067ea8358a195a7419494c5b9e8620cf2
Joe Webster <joseph.scott.webster@gmail.com>
2021-11-04 06:25:55 -0400

    Create in_paragraphs helper method

--- book_viewer.rb
+++ book_viewer.rb
@@ -4,20 +4,29 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+before do
+  @contents = File.readlines('data/toc.txt')
+end
+
 get '/' do
   @title = 'The Adventures of Sherlock Holmes'
-  @contents = File.readlines('data/toc.txt')
 
   erb :home # layout: :layout
 end
 
 get '/chapters/:number' do
-  @contents = File.readlines('data/toc.txt')
-
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
   @title = "Chapter #{number}: #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter # layout: :layout
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
 end
